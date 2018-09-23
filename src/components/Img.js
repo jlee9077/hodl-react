5b2fbe499d787fc88c7e9537be316b07e63d81ab
Joseph Lee <jlee9077@gmail.com>
2018-09-22 21:53:44 -0400

    fixed images

--- src/components/Img.js
+++ src/components/Img.js
@@ -1,14 +1,14 @@
 const images = {
   BTC: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1508609483/bitcoin_eqld4v.png',
-  LTC: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1508609485/ethereum_nw0chu.png',
-  NMC: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1508609486/ripple_p0xeut.png',
-  TRC: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1516327336/bch_2x_hahroi.png',
-  PPC: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1512427497/ltc_fjbqjf.png',
-  NVC: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1508609484/dash_oltvqi.png',
-  FTC: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1508609486/nem_imprip.png',
-  MNC: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1508609486/bitconnect_oj1bo5.png',
-  FRC: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1508609486/monero_wzk3ur.png',
-  IXC: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1508609486/neo_fvoo6c.png',
+  LTC: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Official_Litecoin_Logo.png',
+  NMC: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Namecoin_Coin.png/240px-Namecoin_Coin.png',
+  TRC: 'https://cdn-images-1.medium.com/max/256/1*nnrytOLGbqp0MYo4yVgzag.png',
+  PPC: 'http://icons.iconarchive.com/icons/cjdowner/cryptocurrency/256/Peercoin-icon.png',
+  NVC: 'https://assets.coingecko.com/coins/images/15/large/novacoin.png?1510040290',
+  FTC: 'http://www.therhythmandbluesbrothers.co.uk/coinbaseUK/oscommerce-2.3.3.4/catalog/images/Feathercoin.png',
+  MNC: 'https://s2.coinmarketcap.com/static/img/coins/200x200/9.png',
+  FRC: 'https://s2.coinmarketcap.com/static/img/coins/200x200/10.png',
+  IXC: 'https://s2.coinmarketcap.com/static/img/coins/200x200/13.png',
 };
 // const images = {
 //   BTC: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1508609483/bitcoin_eqld4v.png',
