023f6fd077cc4d0f5f9aa0b260d6f129744c1f3b
Joseph Lee <jlee9077@gmail.com>
2018-09-23 00:32:56 -0400

    added pie chart w/ dummy data

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import {PieChart} from 'react-easy-chart'
+import { PieChart, Legend } from 'react-easy-chart';
+
+
 
 const Graph = props => {
+  const pieData = [
+    {key: 'BitCoin', value: 100, color: '#aaac84'},
+    {key: 'LiteCoin', value: 200, color: '#dce7c5'},
+    {key: 'NameCoin', value: 50, color: '#e3a51a'}
+  ];
+
+  const config = [
+    {color: '#aaac84'},
+    {color: '#dce7c5'},
+    {color: '#e3a51a'}
+  ];
   // const { name, symbol, quotes, circulating_supply } = props.info;
   const { info } = props;
   console.log('Info', info);
-//   return info ? (
-//     <div>
-//       {info.map(i => {
-//         <div>HELLOOOOOOOOO {i.name}</div>;
-//       })}
-//     </div>
-//   ) : (
-//     <div>Loading</div>
-//   );
+  //   return info ? (
+  //     <div>
+  //       {info.map(i => {
+  //         <div>HELLOOOOOOOOO {i.name}</div>;
+  //       })}
+  //     </div>
+  //   ) : (
+  //     <div>Loading</div>
+  //   );
 
-  return info ?(
-    <div>GRAPHING SOON {info.name}</div>
-  ) : <div>Loading</div>
+  return info ? (
+    <div>
+      GRAPHING SOON {info.name}
+      <PieChart data={pieData} size={300} />
+      <Legend data={pieData} dataId={'key'} config={config} horizontal />
+    </div>
+  ) : (
+    <div>Loading</div>
+  );
 };
 
 export default Graph;
